d1a37c88c4e8c5449b06bcd9fb33e7852feef62d
georgeC0stanza <32494929+georgeC0stanza@users.noreply.github.com>
2020-06-10 13:31:14 -0600

    spent too long following unit test tutorials for python that didn't work.

--- tests/test_1.py
+++ tests/test_1.py
@@ -6,9 +6,10 @@ from .models import Bulletins
 
 class UserViewsTests(BaseTestCase):
     def test_users_can_login(self):
-        User.create(name='Joe', email='joe@joes.com', password='12345')
+        User.create(name="Joe", email="joe@joes.com", password="12345")
 
-        response = self.client.post(url_for('users.login'),
-                                    data={'email': 'joe@joes.com', 'password': '12345'})
+        response = self.client.post(
+            url_for("users.login"), data={"email": "joe@joes.com", "password": "12345"}
+        )
 
-        self.assert_redirects(response, url_for('tracking.index'))
+        self.assert_redirects(response, url_for("tracking.index"))
